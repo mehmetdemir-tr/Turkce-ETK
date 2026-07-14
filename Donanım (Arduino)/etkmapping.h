25709ffd95e8bad3d39b834e66b7ba898648b323
Mehmet Demir <mehmetd@vuhuv.com>
2026-07-14 10:09:57 +0300

    Elektronik Semboller Eklendi

--- Donanım (Arduino)/etkmapping.h
+++ Donanım (Arduino)/etkmapping.h
@@ -1,1 +1,27 @@
-#define ETK_OHM
+#ifndef ELEKTRONIK_SEMBOLLER
+#define ELEKTRONIK_SEMBOLLER
+
+#define ETK_OHM "\u2126" // Ω
+#define ETK_SIEMENS "S"
+#define ETK_FAZ "\u03A6" // Φ
+#define ETK_K_FAZ "\u03C6" // φ
+#define ETK_LAMBDA "\u03BB" // λ
+#define ETK_ALFA    "\u03B1" // α
+#define ETK_BETA    "\u03B2" // β 
+#define ETK_GAMA    "\u03B3" // γ
+
+#define ETK_DELTA   "\u0394" // Δ
+#define ETK_MIKRO   "\u03BC" // μ
+#define ETK_PI      "\u03C0" // π
+#define ETK_OMEGA   "\u03C9" // ω
+#define ETK_INTEGRAL "\u222B" // ∫
+#define ETK_SONSUZ  "\u221E" // ∞
+
+#define ETK_DERECE  "\u00B0" // °
+#define ETK_ARTI_EKSI "\u00B1" // ±
+
+#define ETK_AC "\u223C" // ∼
+#define ETK_DC "\u2393" // ⎓
+
+
+#endif
